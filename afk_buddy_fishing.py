cfa0b5b64bc625aee70e968c633d0c8cde00e5ff
sawyer47 <sawyerssandwich07@gmail.com>
2024-11-01 08:21:25 -0500

    made code cleaner and added more documentation

--- afk_buddy_fishing.py
+++ afk_buddy_fishing.py
@@ -10,7 +10,7 @@ def press_and_release(key, duration=0.4):
 
 def hold_key(key, duration=1):
     """Hold a key down for a specified duration."""
-    time.sleep(2) #There is a slow down when you get a fish this corects it.
+    time.sleep(2) #There is a slow down when you get a fish this corects it so you dont get off set..
     with pyautogui.hold(key):
         time.sleep(duration)
 
@@ -20,30 +20,20 @@ print("2")
 time.sleep(1)
 print("1")
 
-while True:
-    hold_key("a")
-    if keyboard.is_pressed("q"):
-        break
+while keyboard.is_pressed("q") == False:
     
-    press_and_release("e")
-    if keyboard.is_pressed("q"):
-        break
+    hold_key("a") # move left
     
-    press_and_release("e")
+    press_and_release("e") #check for fish
+
+    press_and_release("e") # if it has a fish this gets you off the "you chought a fish screen"
     time.sleep(1)
-    press_and_release("c")
-    
+    press_and_release("c") # open the emote wheel to fix a bug when you chatch a fish so you can move
+      # the rest is the same but in reveres 
     hold_key("d")
-    if keyboard.is_pressed("q"):
-        break
     
     press_and_release("e")
-    if keyboard.is_pressed("q"):
-        break
     
     press_and_release("e")
     time.sleep(1)
     press_and_release("c")
-    
-    if keyboard.is_pressed("q"):
-        break
