7aea9c8ebd08264a73099055852d8b6222fcbf82
sawyer47 <sawyerssandwich07@gmail.com>
2024-11-06 12:30:37 -0600

    im so close i just need to find out how to real the rod

--- custom_lib.py
+++ custom_lib.py
@@ -18,3 +18,42 @@ def mouse_click(duration=0.04, LOR="left"): # LOR = left or right mouse botten
     time.sleep(duration)
     pyautogui.mouseUp()
     
+def go_to_lake():
+    hold_key("a", 3.2)
+    hold_key("s", 15)
+    press_and_release("1")
+    
+def get_bait_out():
+    press_and_release("b")
+    time.sleep(0.5)
+    pyautogui.click(x=576, y=485)
+    time.sleep(0.5)
+    press_and_release("b")
+    
+def check_bait():
+        #x=1856, y=1027 90, 117, 90
+    bait_pixel =  pyautogui.pixel(1856, 1027)
+    if bait_pixel != (90, 117, 90):
+        get_more_bait()
+        
+def get_more_bait():
+    press_and_release("w", 14)
+    press_and_release("a", 1)
+    press_and_release("e")
+    pyautogui.click(x=284, y=373)
+    pyautogui.click(x=284, y=373)
+    press_and_release("e")
+    press_and_release("d", 1)
+    press_and_release("s", 14)
+
+def check_for_fish():
+    real = pyautogui.pixel(374, 723)
+    if real != (156, 145, 74):
+        click_fast = True
+    else:
+        click_fast = False
+
+#def catch_fish():
+
+#def check_for_clicks_on_rod():
+    
--- self_fishing_bot.py
+++ self_fishing_bot.py
@@ -3,14 +3,6 @@ import keyboard
 import time
 from custom_lib import *
 # probily going to make it when you load in a world it will just use the known pos of spawn
-def go_to_lake():
-    hold_key("a", 3.2)
-    hold_key("s", 15)
-    press_and_release("1")
-    
-#def start_fidging():
-    
-
 #def go_to_sea():
     
 print(3)
@@ -19,4 +11,6 @@ print(2)
 time.sleep(0.5)
 print(1)
 time.sleep(0.5)
-go_to_lake()
+#go_to_lake()
+#get_bait_out()
+#get_more_bait()
